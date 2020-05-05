23f7fecaada221ff3653f782eb906766d6f9a673
Isaacward97 <Isaac_ward97@hotmail.co.uk>
2020-05-05 01:20:47 +0100

    Created function for balance to be updated with amounts

--- transaction-app/src/AppComponents/IncomeOutgoings.js
+++ transaction-app/src/AppComponents/IncomeOutgoings.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState';
 
 export const IncomeOutgoings = () => {
+  const {transactions} = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction => transaction.amount);
+
+//filters through all the numbers greater than 0 and adds them together using reduce
+   const income = amounts
+     .filter(item => item > 0)
+     .reduce((acc, item) => (acc += item), 0)
+     .toFixed(2);
+
+//similar to income
+   const outgoing = (
+     amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+     -1
+   ).toFixed(2);
+
+
+
   return (
     <div className ="Incomeout-container">
       <div>
         <h4>Income:</h4>
-        <p className ="money add">+£0.00</p>
+        <p className ="money add">{income}</p>
       </div>
       <div>
         <h4>Outgoings:</h4>
-        <p className ="money out">-£0.00</p>
+        <p className ="money out">{outgoing}</p>
       </div>
     </div>
   )
--- transaction-app/src/AppComponents/Transaction.js
+++ transaction-app/src/AppComponents/Transaction.js
@@ -5,7 +5,8 @@ export const Transaction = ({transaction}) => {
   //generates sign based on value beign above or below 0
   const sign = transaction.amount < 0 ? '-' : '+';
   return(
-    <li className="negative">
+    //identifies list component to show red or green colours
+    <li className={transaction.amount < 0 ? 'negative' : 'positive' }>
       {transaction.text} <span>{sign}£{Math.abs(transaction.amount)}</span><button className="remove-btn">x</button>
     </li>
   )
